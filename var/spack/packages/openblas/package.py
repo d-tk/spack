987baa315ab3a16c36d09e2d98b5734b1485ebb5
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2015-12-16 15:22:40 +0100

    fix openblas

--- var/spack/packages/openblas/package.py
+++ var/spack/packages/openblas/package.py
@@ -9,14 +9,13 @@ class Openblas(Package):
     url      = "http://github.com/xianyi/OpenBLAS/archive/v0.2.15.tar.gz"
 
     version('0.2.15', 'b1190f3d3471685f17cfd1ec1d252ac9')
-    version('develop', git='https://github.com/xianyi/OpenBLAS.git', branch='develop')
     version('0.2.14', '53cda7f420e1ba0ea55de536b24c9701')
-
-    version('master', git="https://github.com/xianyi/OpenBLAS.git", branch = 'develop')
+    version('develop', git='https://github.com/xianyi/OpenBLAS.git', branch='develop')
 
     variant('mt', default=False, description="Use Multithreaded version")
     variant('openmp', default=False, description="Use Multithreaded version with OpenMP compatibility")
     variant('shared', default=False, description='Enable shared library')
+    variant('dynamic', default=True, description='Enable dynamic architecture')
 
     provides('blas')
     provides('cblas')
@@ -38,37 +37,42 @@ class Openblas(Package):
             module.cblaslibname=[os.path.join(self.spec.prefix.lib, "libopenblas.a")]
             module.lapacklibname=[os.path.join(self.spec.prefix.lib, "libopenblas.a")]
 
+        if spec.satisfies('+mt'):
+             module.blaslibname+=["-lpthread"]
         module.blaslibfortname = module.blaslibname
         module.cblaslibfortname = module.cblaslibname
         module.lapacklibfortname = module.lapcklibname
-        if spec.satisfies('+mt'):
-            module.blaslibname+=["-lpthread"]
+
 
     def install(self, spec, prefix):
-          options='BINARY=64 DYNAMIC_ARCH=1'
+        options=['BINARY=64']
 
         if spec.satisfies('+mt'):
-            options+=' USE_THREAD=1'
+            options.append('USE_THREAD=1')
         else:
-            options+=' USE_THREAD=0'
+            options.append('USE_THREAD=0')
 
         if spec.satisfies('+openmp'):
-            options+=' USE_OPENMP=1'
+            options.append('USE_OPENMP=1')
+
+        if spec.satisfies('+dynamic'):
+            options.append('DYNAMIC_ARCH=1')
 
         if spec.satisfies('+shared'):
-            options+=' NO_STATIC=1'
+            options.append('NO_STATIC=1')
         else:
-            options+=' NO_SHARED=1'
+            options.append('NO_SHARED=1')
 
         if spec.satisfies('%gcc'):
-            options+=' FC=gfortran'
+            options.append('FC=gfortran')
 
         if spec.satisfies('%mkl'):
-            options+=' FC=ifort'
+            options.append('FC=ifort')
 
-        make('%s'%options)
+        make(*options)
 
-        # make install needs the other parameters as well
-        options+=' PREFIX='+prefix+ ' install'
+        # make install needs the other parameters as well (?)
+        options.append("PREFIX="+prefix)
+        options.append("install")
 
-        make('%s'%options, parallel=False)
+        make(*options, parallel=False)
