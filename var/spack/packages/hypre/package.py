ab84deb7abaeb3f6a694f4748cb749a39a275a63
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2016-03-07 11:29:01 +0100

    hypre: add double underscore variant

--- var/spack/packages/hypre/package.py
+++ var/spack/packages/hypre/package.py
@@ -11,6 +11,8 @@ class Hypre(Package):
 
     version('2.10.0b', '768be38793a35bb5d055905b271f5b8e')
 
+    variant('double_underscore', default=False, description='Enable double underscore option')
+
     depends_on("mpi")
     depends_on("blas")
     depends_on("lapack")
@@ -25,13 +27,19 @@ class Hypre(Package):
 
             os.environ['CFLAGS']   = "-g -O2 -fPIC"
             os.environ['CXXFLAGS'] = "-g -O2 -fPIC"
-                                             
-            configure(
+
+            config_args = [
                 "--prefix=%s" % prefix,
+                "--enable-bigint",
                 "--with-blas-libs=blas",
                 "--with-blas-lib-dirs=%s/lib" % blas_dir,
                 "--with-lapack-libs=\"lapack blas\"",
                 "--with-lapack-lib-dirs=%s/lib" % lapack_dir,
-                "--with-MPI")
+                "--with-MPI" ]
+
+            if spec.satisfies('+double_underscore'):
+                config_args.append("--with-fmangle=two-underscores")
+
+            configure(*config_args)
             make()
             make("install")
